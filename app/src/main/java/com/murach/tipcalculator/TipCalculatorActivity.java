f306622c8f21e716f0f790329dd0d88bab1c7f0c
AMD <jyorkcar@sbcglobal.net>
2017-10-06 22:29:25 -0700

    Ch5.2 Radio buttons and spinner.

--- app/src/main/java/com/murach/tipcalculator/TipCalculatorActivity.java
+++ app/src/main/java/com/murach/tipcalculator/TipCalculatorActivity.java
@@ -7,8 +7,11 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.app.Activity;
@@ -26,7 +29,22 @@ implements OnEditorActionListener, OnClickListener {
     private TextView tipTextView;
     private TextView totalTextView;
     private Button   applyButton;
-    
+    private TextView    textViewPerPersonAmt,
+                        textViewPerPerson;
+
+
+    //RadioButtons
+    private RadioButton rbNone,
+                        rbTip,
+                        rbTotal;
+
+
+    private ArrayAdapter<CharSequence> adapter;
+    private Spinner sp;
+
+    private float splitAmt,
+                  splitWays;//Divisor.
+
     // define the SharedPreferences object
     private SharedPreferences savedValues;
     
@@ -47,6 +65,22 @@ implements OnEditorActionListener, OnClickListener {
         tipTextView = (TextView) findViewById(R.id.tipTextView);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
         applyButton = (Button) findViewById(R.id.applyButton);
+
+        //Per-person split.
+        textViewPerPersonAmt = (TextView)findViewById(R.id.textViewPerPersonAmt);
+
+        textViewPerPerson  = (TextView)findViewById(R.id.textViewPerPerson);
+
+        //RadioButton refs.
+        rbNone      = (RadioButton)findViewById(R.id.radioButtonNone);
+        rbTip       = (RadioButton)findViewById(R.id.radioButtonTip);
+        rbTotal     = (RadioButton)findViewById(R.id.radioButtonTotal);
+
+        //Setup the spinner with adapter.
+        sp = (Spinner)findViewById(R.id.spinnerSplitBill);
+        adapter = ArrayAdapter.createFromResource(this,R.array.split_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
 
         // set the listeners
         billAmountEditText.setOnEditorActionListener(this);
@@ -99,14 +133,81 @@ implements OnEditorActionListener, OnClickListener {
         // calculate tip and total 
         float tipAmount = billAmount * tipPercent;
         float totalAmount = billAmount + tipAmount;
-        
+
+        if(rbNone.isChecked())
+        {
+
+        }
+        else if(rbTip.isChecked())
+        {
+            tipAmount       = StrictMath.round(billAmount * tipPercent);
+            totalAmount     = billAmount + tipAmount;
+        }
+        else if(rbTotal.isChecked())
+        {
+            float tipNotRounded         = billAmount * tipPercent;
+            totalAmount                 = StrictMath.round(billAmount + tipNotRounded);
+            tipAmount                   = totalAmount - billAmount;
+        }
+
+
+
+        switch(sp.getSelectedItemPosition())
+        {
+            case 0://No split.
+                splitWays=1;
+
+                //Hide Per-person widgets.
+                textViewPerPersonAmt.setVisibility(View.INVISIBLE);
+                textViewPerPerson.setVisibility(View.INVISIBLE);
+            break;
+
+            case 1://2-way split.
+                splitWays=2;
+
+                //Show per-person widgets.
+                textViewPerPersonAmt.setVisibility(View.VISIBLE);
+                textViewPerPerson.setVisibility(View.VISIBLE);
+            break;
+
+            case 2://3-way split.
+                splitWays=3;
+
+                //Show per-person widgets.
+                textViewPerPersonAmt.setVisibility(View.VISIBLE);
+                textViewPerPerson.setVisibility(View.VISIBLE);
+            break;
+
+            case 3://4-way split.
+                splitWays=4;
+
+                //Show per-person widgets.
+                textViewPerPersonAmt.setVisibility(View.VISIBLE);
+                textViewPerPerson.setVisibility(View.VISIBLE);
+            break;
+
+            default:
+                splitWays=1;
+
+                //Hide Per-person widgets.
+                textViewPerPersonAmt.setVisibility(View.INVISIBLE);
+                textViewPerPerson.setVisibility(View.INVISIBLE);
+        }
+
+        splitAmt = totalAmount / splitWays;
+
         // display the other results with formatting
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         tipTextView.setText(currency.format(tipAmount));
         totalTextView.setText(currency.format(totalAmount));
-        
+
+
+        textViewPerPersonAmt.setText(currency.format(splitAmt));
+
+
         NumberFormat percent = NumberFormat.getPercentInstance();
         percentTextView.setText(percent.format(tipPercent));
+
     }
     
     @Override
@@ -123,10 +224,16 @@ implements OnEditorActionListener, OnClickListener {
         switch (v.getId()) {
         case R.id.percentDownButton:
             tipPercent = tipPercent - .01f;
+
+            rbNone.setChecked(true);
+
             calculateAndDisplay();
             break;
         case R.id.percentUpButton:
             tipPercent = tipPercent + .01f;
+
+            rbNone.setChecked(true);
+
             calculateAndDisplay();
             break;
         case R.id.applyButton:
